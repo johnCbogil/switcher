3ed92b53f7fd4fd148eef4ea11d100a97151eda8
John Bogil <johncbogil@gmail.com>
2023-02-04 11:07:27 -0500

    Initial Commit

---
+++ Switcher/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Switcher
+//
+//  Created by John Bogil on 2/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Switcher/SwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitcherApp.swift
+//  Switcher
+//
+//  Created by John Bogil on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
