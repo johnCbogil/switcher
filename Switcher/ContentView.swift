26d719802eebd648c7cb18fc5b4974c272942a0e
John Bogil <johncbogil@gmail.com>
2023-02-04 14:34:10 -0500

    track total counting

--- Switcher/ContentView.swift
+++ Switcher/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.scenePhase) private var phase
+    @State var openCount = 0
+    private let defaults = UserDefaults.standard
+    init() {
+        openCount = defaults.integer(forKey: "openCount")
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "arrow.uturn.up")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Switcher App")
+            Text("Total Open Count = \(openCount)")
         }
         .padding()
+        .onChange(of: phase) { newPhase in
+            switch newPhase {
+            case .active:
+                openCount += 1
+                defaults.set(openCount, forKey: "openCount")
+            default:
+                break
+            }
+        }
     }
 }
 
--- Switcher/SwitcherApp.swift
+++ Switcher/SwitcherApp.swift
@@ -9,18 +9,10 @@ import SwiftUI
 
 @main
 struct SwitcherApp: App {
-    @Environment(\.scenePhase) private var phase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .onChange(of: phase) { newPhase in
-            switch newPhase {
-            case .active: print("we switchin")
-            default: break
-            }
-        }
-
     }
 }
